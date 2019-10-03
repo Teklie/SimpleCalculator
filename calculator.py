acdd978dd2a826ac933c3d0b6b9208f0909d07e1
Book <tekliebelay@gmail.com>
2019-10-03 11:19:27 +0200

    Functions for addition and subtraction added

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
 """
-Calculator library containing basic math operations.
+Simple calculator containing basic math operations.
 """
 
 def add(first_number, second_number):
     return first_number + second_number
 
-def subtract(first_number second_number):
+
+def subtract(first_number, second_number):
     return first_number - second_number
