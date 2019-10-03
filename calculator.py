9bac5f13c03d659c85a78214b2d0a716fa701f5a
Book <tekliebelay@gmail.com>
2019-10-03 12:13:16 +0200

    Multiply function is added

--- calculator.py
+++ calculator.py
@@ -8,3 +8,6 @@ def add(first_number, second_number):
 
 def subtract(first_number, second_number):
     return first_number - second_number
+
+def multiply(first_number, second_number):
+    return first_number * second_number
